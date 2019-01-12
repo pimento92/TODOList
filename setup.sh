ced4ae71f681021039d3f1b393f5ac9666ab10be
Angel Lillo Amaro <angel.lillo.amaro@gmail.com>
2019-01-12 16:31:36 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
 #Cambiar permisos
 chmod -R 777 ../html
 #Instalar mysql
 sudo apt-get install mysql-server
 sudo mysql_secure_installation
 #Instalar BD
-mysql --host=localhost --user=todolistdba --password=todolistpass  -e "Models\BD_TODOLIST.sql"
+mysql --host=localhost --user=root --pass=root -e "Models\BD_TODOLIST.sql"
